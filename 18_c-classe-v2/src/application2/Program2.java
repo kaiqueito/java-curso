7b3651ecace42ab27b6fda11ce6da1e24af1db32
= <=>
2023-05-10 13:16:18 -0300

    estudos

---
+++ 18_c-classe-v2/src/application2/Program2.java
@@ -0,0 +1,43 @@
+package application2;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities2.Triangle2;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle2 x, y;
+		
+		x = new Triangle2();
+		y = new Triangle2();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Triangle X area: %.2f%n", areaX);
+		System.out.printf("Triangle Y area: %.2f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+		
+		sc.close();
+	}
+
+}
